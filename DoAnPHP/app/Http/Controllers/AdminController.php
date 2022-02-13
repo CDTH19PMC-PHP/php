e62f2dcce482d283c2925bd76861d4b39e9ff514
Trần Quang Thiện <0306191468@caothang.edu.vn>
2022-02-13 15:09:26 +0700

    Tran Quang Thien

--- DoAnPHP/app/Http/Controllers/AdminController.php
+++ DoAnPHP/app/Http/Controllers/AdminController.php
@@ -107,16 +107,149 @@ class AdminController extends Controller
 
         return redirect()->back()->with("success","Mật khẩu đã được thay đổi thành công!");
     }
-    // danh sách quản lý Giáo Viên
+    //////////////// danh sách quản lý Giáo Viên ///////////////////////
     public function tableDataGV(){
         $dsGiaoVien = GiaoVien::all();
         //$giaoVien = DB::table('giao_vien')->get();
         return view('dashboard.table-data-gv',compact('dsGiaoVien'));
     }
-    // danh sách quản lý Học Sinh
+    public function deleteDSGiaoVienAD($id){
+        $gv = GiaoVien::find($id);
+        $gv->trang_thai = 0;
+        $gv->save();
+        $dsGiaoVien = GiaoVien::all();
+       
+        return view('admin-quan-ly-gv',compact('dsGiaoVien'));
+        // return redirect()->route('admin-quan-ly-gv',$dsGiaoVien);
+    }
+
+    // Chỉnh sửa thông tin cá nhân GV (Admin)
+    public function formInformationGV($id){
+        $editgv = GiaoVien::find($id);
+        return view('admin-quan-ly-gv-edit-form',compact('editgv'));
+    }
+    public function editInformationGV(Request $request,$id){
+        $editGiaoVien = GiaoVien::find($id);
+        $editGiaoVien->username = $request->username;
+        $editGiaoVien->ho_ten = $request->ho_ten;
+        $editGiaoVien->ngay_sinh = $request->ngay_sinh;
+        $editGiaoVien->dia_chi = $request->dia_chi;
+        $editGiaoVien->so_dien_thoai = $request->so_dien_thoai;
+        $editGiaoVien->save();
+
+        $editgv = GiaoVien::find($id);
+
+        return view('admin-quan-ly-gv-info',compact('editgv'));
+    }
+
+    public function formResetPasswordGV($id){
+        $resetPassword = GiaoVien::find($id);
+        return view('admin-quan-ly-gv-reset-password-form',compact('resetPassword'));
+    }
+
+    public function resetPasswordGV($id){
+        $resetPasswordGiaoVien = GiaoVien::find($id);
+        $resetPasswordGiaoVien->password = Hash::make('123456789');
+        $resetPasswordGiaoVien->save();
+
+        $resetPassword = GiaoVien::find($id);
+        return view('admin-quan-ly-gv-reset-password',compact('resetPassword'));
+    }
+    //Add Giáo viên vào danh sách quản lý
+    public function addGVform(){
+        return view('admin-quan-ly-gv-add');
+    }
+    public function handleAddGV(Request $request){
+        $giaoVien = GiaoVien::where('username',$request->username)->first();
+        if($giaoVien == true){
+            return redirect()->back()->with("error","Tài khoản đã tồn tại.");
+        }
+        $gv = new GiaoVien();
+        $gv->username = $request->username;
+        $gv->password = Hash::make('123456789');
+        // $gv->ho_ten=$request->ho_ten;
+        // $gv->ngay_sinh=$request->ngay_sinh;
+        // $gv->dia_chi=$request->dia_chi;
+        // $gv->so_dien_thoai=$request->so_dien_thoai;
+        $gv->ho_ten='Nguyễn Văn A';
+        $gv->ngay_sinh='1/1/1900';
+        $gv->dia_chi='Tp.HCM';
+        $gv->so_dien_thoai='0';
+        $gv->trang_thai = 1;
+        $gv->save();
+        return redirect()->back()->with("success","Thêm tài khoản giáo viên thành công!");
+    }
+    ////////////////////// danh sách quản lý Học Sinh ////////////////////
     public function tableDataHS(){
         $dsHocSinh = HocSinh::all();
         //$giaoVien = DB::table('giao_vien')->get();
         return view('dashboard.table-data-hs',compact('dsHocSinh'));
+    }//Xóa Giáo Viên trong ds GV (change trạng thái = 0)
+    public function deleteDSHocSinhAD($id){
+        $hs = HocSinh::find($id);
+        $hs->trang_thai = 0;
+        $hs->save();
+        $dsHocSinh = HocSinh::all();
+       
+        return view('admin-quan-ly-hs',compact('dsHocSinh'));
+        // return redirect()->route('admin-quan-ly-gv',$dsGiaoVien);
     }
+
+    // Chỉnh sửa thông tin cá nhân GV (Admin)
+    public function formInformationHS($id){
+        $ediths = HocSinh::find($id);
+        return view('admin-quan-ly-hs-edit-form',compact('ediths'));
+    }
+    public function editInformationHS(Request $request,$id){
+        $editHocSinh = HocSinh::find($id);
+        $editHocSinh->username = $request->username;
+        $editHocSinh->ho_ten = $request->ho_ten;
+        $editHocSinh->ngay_sinh = $request->ngay_sinh;
+        $editHocSinh->dia_chi = $request->dia_chi;
+        $editHocSinh->so_dien_thoai = $request->so_dien_thoai;
+        $editHocSinh->save();
+
+        $ediths = HocSinh::find($id);
+
+        return view('admin-quan-ly-hs-info',compact('ediths'));
+    }
+
+    public function formResetPasswordHS($id){
+        $resetPassword = HocSinh::find($id);
+        return view('admin-quan-ly-hs-reset-password-form',compact('resetPassword'));
+    }
+
+    public function resetPasswordHS($id){
+        $resetPasswordHocSinh = HocSinh::find($id);
+        $resetPasswordHocSinh->password = Hash::make('123456789');
+        $resetPasswordHocSinh->save();
+
+        $resetPassword = HocSinh::find($id);
+        return view('admin-quan-ly-hs-reset-password',compact('resetPassword'));
+    }
+    //Add Giáo viên vào danh sách quản lý
+    public function addHSform(){
+        return view('admin-quan-ly-hs-add');
+    }
+    public function handleAddHS(Request $request){
+        $hocSinh = HocSinh::where('username',$request->username)->first();
+        if($hocSinh == true){
+            return redirect()->back()->with("error","Tài khoản đã tồn tại.");
+        }
+        $hs = new HocSinh();
+        $hs->username = $request->username;
+        $hs->password = Hash::make('123456789');
+        // $gv->ho_ten=$request->ho_ten;
+        // $gv->ngay_sinh=$request->ngay_sinh;
+        // $gv->dia_chi=$request->dia_chi;
+        // $gv->so_dien_thoai=$request->so_dien_thoai;
+        $hs->ho_ten='Nguyễn Văn A';
+        $hs->ngay_sinh='1/1/1900';
+        $hs->dia_chi='Tp.HCM';
+        $hs->so_dien_thoai='0';
+        $hs->trang_thai = 1;
+        $hs->save();
+        return redirect()->back()->with("success","Thêm tài khoản học sinh thành công!");
+    }
+    
 }
--- DoAnPHP/routes/web.php
+++ DoAnPHP/routes/web.php
@@ -52,36 +52,59 @@ Route::get('/admin/{id}',[AdminController::class,'DanhSachLop'])->name('admin-ds
 
 ///////////////// Trần Quang Thiện ////////////
 ///////////////// Trần Quang Thiện ////////////
-Route::get('/dang-nhap', [HocSinhController::class,'dangNhapHS'])->name('dang-nhap-hs');
-Route::post('/dang-nhap', [HocSinhController::class,'xuLyDangNhapHS'])->name('xl-dang-nhap-hs');
-//
-Route::get('/dang-nhap-giaovien', [GiaoVienController::class,'dangNhapGV'])->name('dang-nhap-gv');
-Route::post('/dang-nhap-giaovien', [GiaoVienController::class,'xuLyDangNhapGV'])->name('xl-dang-nhap-gv');
-//
-Route::get('/dang-nhap-admin', [AdminController::class,'dangNhapAD'])->name('dang-nhap-ad');
-Route::post('/dang-nhap-admin', [AdminController::class,'xuLyDangNhapAD'])->name('xl-dang-nhap-ad');
-//
-Route::get('/admin/thong-tin-ca-nhan/{id}',[AdminController::class,'FormThongTinCaNhanAD'])->name('form-thong-tin-ca-nhan-ad');
-Route::post('/admin/thong-tin-ca-nhan/{id}',[AdminController::class,'XuLyThongTinCaNhanAD'])->name('xl-chinh-sua-tt-ad');
-//
-Route::get('/form-admin-thay-doi-pw/{id}',[AdminController::class,'changePW'])->name('changePW');
-// Route::match(['get','post'], '/form-admin-thay-doi-pw/{id}',[AdminController::class,'update-pw'])->name('update-pw');
-Route::post('/form-admin-thay-doi-pw/{id}',[AdminController::class,'updatePW'])->name('update-pw');
 
-//=========== Quản lý Giáo Viên =========== //
-// Danh sách giáo viên
-Route::get('/admin-quan-ly-gv',[AdminController::class,'dsGiaoVienAD'])->name('ds-quan-ly-gv');
-// Thêm giáo viên
-//=========== Quản lý Học Sinh =========== //
-// Danh sách học sinh
-Route::get('/admin-quan-ly-hs',[AdminController::class,'dsHocSinhAD'])->name('ds-quan-ly-hs');
+///////////////// Học Sinh (Admin) ////////////
+    Route::get('/dang-nhap', [HocSinhController::class,'dangNhapHS'])->name('dang-nhap-hs');
+    Route::post('/dang-nhap', [HocSinhController::class,'xuLyDangNhapHS'])->name('xl-dang-nhap-hs');
+            ///// Danh sách học sinh /////
+    Route::get('/table-data-hs',[AdminController::class,'tableDataHS'])->name('table-data-hs');
+    // Thêm học sinh
+    Route::get('/admin-quan-ly-hs-add',[AdminController::class,'addGVform'])->name('add-hs-form');
+    Route::post('/admin-quan-ly-hs-add',[AdminController::class,'handleAddHS'])->name('handle-add-hs');
+    // Xóa
+    Route::get('/admin-quan-ly-hs/{id}',[AdminController::class,'deleteDSHocSinhAD'])->name('xoa-ds-quan-ly-hs');
+    // Sửa
+    Route::get('/admin-quan-ly-hs-edit/{id}',[AdminController::class,'formInformationHS'])->name('form-information-hs');
+    Route::post('/admin-quan-ly-hs-edit/{id}',[AdminController::class,'editInformationHS'])->name('edit-information-hs');
+    // Reset password
+    Route::get('/admin-quan-ly-reset-password-hs/{id}',[AdminController::class,'formResetPasswordHS'])->name('form-reset-password-hs');
+    Route::post('/admin-quan-ly-reset-password-hs/{id}',[AdminController::class,'resetPasswordHS'])->name('reset-password-hs');
+
+///////////////// Giáo Viên (Admin) ////////////
+    Route::get('/dang-nhap-giaovien', [GiaoVienController::class,'dangNhapGV'])->name('dang-nhap-gv');
+    Route::post('/dang-nhap-giaovien', [GiaoVienController::class,'xuLyDangNhapGV'])->name('xl-dang-nhap-gv');
+            ///// Danh sách giáo viên /////
+    Route::get('/table-data-gv',[AdminController::class,'tableDataGV'])->name('table-data-gv');
+    // Thêm giáo viên
+    Route::get('/admin-quan-ly-gv-add',[AdminController::class,'addGVform'])->name('add-gv-form');
+    Route::post('/admin-quan-ly-gv-add',[AdminController::class,'handleAddGV'])->name('handle-add-gv');
+    // Xóa
+    Route::get('/admin-quan-ly-gv/{id}',[AdminController::class,'deleteDSGiaoVienAD'])->name('xoa-ds-quan-ly-gv');
+    // Sửa
+    Route::get('/admin-quan-ly-gv-edit/{id}',[AdminController::class,'formInformationGV'])->name('form-information-gv');
+    Route::post('/admin-quan-ly-gv-edit/{id}',[AdminController::class,'editInformationGV'])->name('edit-information-gv');
+    // Reset password
+    Route::get('/admin-quan-ly-reset-password-gv/{id}',[AdminController::class,'formResetPasswordGV'])->name('form-reset-password-gv');
+    Route::post('/admin-quan-ly-reset-password-gv/{id}',[AdminController::class,'resetPasswordGV'])->name('reset-password-gv');
+
+//////////////// Admin ////////////
+    Route::get('/page-login',[AdminController::class,'loginAD'])->name('login-ad');
+    Route::post('/page-login', [AdminController::class,'xuLyloginAD'])->name('xl-login-ad');
+            ///// Index Admin /////
+    Route::get('/index-admin',[AdminController::class,'indexAD'])->name('index-ad');
+            ///// Thông Tin Cá Nhân Admin /////
+    Route::get('/admin/thong-tin-ca-nhan/{id}',[AdminController::class,'FormThongTinCaNhanAD'])->name('form-thong-tin-ca-nhan-ad');
+    Route::post('/admin/thong-tin-ca-nhan/{id}',[AdminController::class,'XuLyThongTinCaNhanAD'])->name('xl-chinh-sua-tt-ad');
+            ///// Thay Đổi Mật Khẩu Admin /////
+    Route::get('/form-admin-thay-doi-pw/{id}',[AdminController::class,'changePW'])->name('changePW');
+    Route::post('/form-admin-thay-doi-pw/{id}',[AdminController::class,'updatePW'])->name('update-pw');
+
 ///////////////// Trần Quang Thiện ////////////
 ///////////////// Trần Quang Thiện ////////////
-Route::get('/page-login',[AdminController::class,'loginAD'])->name('login-ad');
-Route::post('/page-login', [AdminController::class,'xuLyloginAD'])->name('xl-login-ad');
 
-Route::get('/index-admin',[AdminController::class,'indexAD'])->name('index-ad');
 
-Route::get('/table-data-gv',[AdminController::class,'tableDataGV'])->name('table-data-gv');
 
-Route::get('/table-data-hs',[AdminController::class,'tableDataHS'])->name('table-data-hs');
+
+
+
+
